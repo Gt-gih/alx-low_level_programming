facd1a1758f3a097008b117e5aef67f716013d0f
gitman <girmaytekle84@gmail.com>
2022-09-09 16:24:37 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print the no is pos, neg or zero
+ *
+ * Description using the main function
+ * Return:0
+ */
+int main(void)
+{
+char c;
+for (c = 'z' ; c >= 'a' ; c--)
+{
+	putchar (c);
+}
+putchar ('\n');
+return (0);
+}
