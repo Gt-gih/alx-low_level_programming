dd36adc9ccdc0f03139f004bb6756534382c9dc0
gitman <girmaytekle84@gmail.com>
2022-09-08 17:11:54 -0700

    c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main
- *
- * description
- * this program
- * return:
+ * main -entry point
+ * 
+ * Return: 0 after printing the function 
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle");
         return (0);
 }
