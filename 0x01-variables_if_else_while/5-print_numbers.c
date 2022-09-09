afe492bf18cf6ca2b832e70875d21a64002cfc58
gitman <girmaytekle84@gmail.com>
2022-09-09 15:57:45 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print the no is pos, neg or zero
+ *
+ * Description using the main function
+ * Return:0
+ */
+int main(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+{
+	printf ("%i", i);
+}
+print ("\n");
+return (0);
+}
