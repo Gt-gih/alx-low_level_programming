5c8560155c4e0fae244e37996c72fc1c846f6ae9
Gt-gih <111019579+Gt-gih@users.noreply.github.com>
2022-09-15 01:58:17 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
 void print_alphabet(void)
 {
 	char c;
