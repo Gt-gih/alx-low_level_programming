de1ba115840b44fe458b4894be7d1686e5d45b72
Girmay <girmaytekle84@gmail.com>
2022-09-15 02:20:24 +0300

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * return: always success
+ *
+ */
+void print_alphabet_x10(void)
+{
+
+int co;	
+char c;
+co = 0;
+
+while(co < 10)
+{
+
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+
+co++;
+
+_putchar('\n');
+}
+
+}
