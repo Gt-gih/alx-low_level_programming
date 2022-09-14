be9e97b10ef52f0f74119b253f23665ce12fa378
Girmay <girmaytekle84@gmail.com>
2022-09-14 23:55:49 +0300

    task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-#include <main.c>
+#include <time.c>
+#include <stdlib.h>
+#include "main.h"
 
 /**
  * main -entry point
@@ -11,14 +13,15 @@
 
 int main(void)
 {
-	_putchar("_")
-	_putchar("p")
-	_putchar("u")
-	_putchar("t")
-	_putchar("c")
-	_putchar("h")
-	_putchar("a")
-	_putchar("r")
+	_putchar('_')
+	_putchar('p')
+	_putchar('u')
+	_putchar('t')
+	_putchar('c')
+        _putchar('h')
+        _putchar('a')
+        _putchar('r')
+	_putchar('\n')
 
 	return (0);
 }
