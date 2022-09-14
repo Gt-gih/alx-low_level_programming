d69a8b785e167b00396bd9689f5ff4b48053f1f4
Girmay <girmaytekle84@gmail.com>
2022-09-15 00:39:13 +0300

    task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
 * main -entry point
 *
@@ -8,11 +7,9 @@
 *
 * return: 0 upon success
 */
-
 int main(void)
 {
-	printf("_putchar\n");
+printf("_putchar\n");
 
-	return (0);
-
+return (0);
 }
