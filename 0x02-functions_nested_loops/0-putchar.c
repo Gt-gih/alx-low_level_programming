94c3fd9dbbbdc91ae44b6f871654434ee97ff333
Girmay <girmaytekle84@gmail.com>
2022-09-15 00:36:58 +0300

    task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * main -entry point
- *
- * description -prints using _putchar function
- *
- * return: 0 upon success
- */
+* main -entry point
+*
+* description -prints using _putchar function
+*
+* return: 0 upon success
+*/
 
 int main(void)
 {
