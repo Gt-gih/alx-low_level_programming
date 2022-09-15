444188371cffd6b4c2117a9bdd2d5944a4a9c073
Girmay <girmaytekle84@gmail.com>
2022-09-15 04:58:36 +0300

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,11 +15,11 @@ void times_table(void)
 
 		for (mult = 0; mult <= 9; mult++)
 		{
-			-putchar(',');
-			-putchar(' ');
+			_putchar(',');
+			_putchar(' ');
 
 			prod = num * mult;
-			if (pod <= 9)
+			if (prod <= 9)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
