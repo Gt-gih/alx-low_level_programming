216a7d46cb84939b6c0e32aa848f14669c6900ee
Girmay <girmaytekle84@gmail.com>
2022-09-15 04:55:59 +0300

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * times_table- Entry point
+ * Descriptiomn init parameter
+ * Return:0
+ *
+ */
+void times_table(void)
+{
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (mult = 0; mult <= 9; mult++)
+		{
+			-putchar(',');
+			-putchar(' ');
+
+			prod = num * mult;
+			if (pod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+			
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
