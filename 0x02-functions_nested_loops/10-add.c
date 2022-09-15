08b934aa63ad98e1fc60a08d5c1ccd43b25e84da
Girmay <girmaytekle84@gmail.com>
2022-09-15 05:12:13 +0300

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add- Entry point
+ * @num1- parameter 1 
+ * @num2- parameter 2
+ * Descriptiomn init parameter
+ * Return:0
+ *
+ */
+int add(int num2, int num1)
+{
+	return (num1 + num2);
+}
