77ec26cb7f62a29de88109d62b8eabddc6eed6d3
Girmay <girmaytekle84@gmail.com>
2022-09-16 23:17:44 +0300

    task 2 added

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer to be multiplayed
+ * @b: second integer to be multiplayed
+ *
+ * Return: multiplication of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
