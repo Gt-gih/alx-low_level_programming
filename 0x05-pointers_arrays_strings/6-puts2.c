ba398277d37bc26bfbf4858f4cca88fc8ed33868
Girmay <girmaytekle84@gmail.com>
2022-09-19 23:27:31 +0300

    task 6 added

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
 
 /**
  * puts2 - Prints one char out of two of a string.
@@ -6,10 +8,8 @@
  */
 void puts2(char *str)
 {
-	int i, len = 0;
-
-	while (str[i++])
-		len++;
+	int len = strlen(s);
+	int i;
 
 	for (i = 0; i < len; i += 2)
 		_putchar(str[i]);
