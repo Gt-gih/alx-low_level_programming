ba1247914687ad712dcba1c080d96f84ef3a7c01
Girmay <girmaytekle84@gmail.com>
2022-09-27 00:13:29 +0300

    task 3 added

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,12 +6,12 @@
  * @s: param that holds a char to print
  * @accept: param for char
  *
- * Return: i
+ * Return: always 0 on success
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int count;
+	int count;
 
 	while (*s != '\0')
 	{
