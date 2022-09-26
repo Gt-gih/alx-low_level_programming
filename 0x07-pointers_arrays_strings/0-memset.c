fd751ad8d23a3a5bb7d4bc507444aa9b79e592f1
Girmay <girmaytekle84@gmail.com>
2022-09-26 23:16:17 +0300

    task 0 added

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,19 +2,19 @@
 
 /**
  * _memset - handles memory with a constant value.
- * @s: var to allocated memory - array of chars.
- * @b: var of char bytes.
+ * @d: var to allocated memory - array of chars.
+ * @s: var of char bytes.
  * @n: number of spaces to be filld .
  *
  * Return: pointer to memory area s
  */
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *d, char *s, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		*(s + i) = b;
+		s[i] = b[i];
 	}
-	return (s);
+	return (d);
 }
