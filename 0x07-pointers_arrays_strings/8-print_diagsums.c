73fdf1c61e36ab148aa3587135883a9794c5e833
Girmay <girmaytekle84@gmail.com>
2022-09-27 09:15:51 +0300

    task 8 added

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,7 +11,10 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, n, total1 = 0, total2 = 0;
+	int i;
+	int n;
+	int total1 = 0;
+	int total2 = 0;
 
 	for (i = 0; i <= (size * size);i = i + size + 1)
 		total1 = total1 + a[i];
