5d7a7d5e6cb4ba4cb8c767d61868eac83644dcc3
gitman <girmaytekle84@gmail.com>
2022-10-02 07:29:42 -0700

    task 0 added

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,19 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * main - function to accept my name is arguments
- * Return: 0
+ *
  * @argc: amount of args
  * @argv: pointer to pointers array
+ *
+ * Return: 0 on success
  */
+
 int main(int argc, char *argv[])
 {
-
 	int c = 0;
-
+	
 	while (c < argc)
 	{
 		if (c == 0)
