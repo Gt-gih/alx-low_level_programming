393ab41e7da3b029d6aebb85cdeb6ac632172a8f
Girmay <girmaytekle84@gmail.com>
2022-10-10 23:27:31 +0300

    task 2 added

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,21 +1,24 @@
 #include "dog.h"
-#include <stdlib.h>
+#include <stdio.h>
 /**
- * init_dog - writes the character c to stdout
- * @d: Dog
- * @name: Dog name
- * @age: dog age
- * @owner: Dog owner
+ * print_dog - prints a struct dog
+ * @d: Pointer to struct
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void print_dog(struct dog *d)
 {
 	if (d != 0)
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+		if ((*d).name != 0)
+			printf("Name: %s\n", (*d).name);
+		else
+			printf("Name: (nil)\n");
+		printf("Age: %f\n", (*d).age);
+		if ((*d).owner != 0)
+			printf("Owner: %s\n", (*d).owner);
+		else
+			printf("Owner: (nil)\n");
 	}
 }
